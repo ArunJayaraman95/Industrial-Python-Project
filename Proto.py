7aa6d48728bd8e1c9aaf8ad0a9f3f0a850136ed9
Arun Jayaraman <riparun.aj@gmail.com>
2023-04-22 10:17:25 -0400

    Update ticker function and formatting

--- Proto.py
+++ Proto.py
@@ -2,109 +2,136 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
 from PyQt5.uic import loadUi
-from bs4 import BeautifulSoup as bs
 from dotenv import load_dotenv
 import os
-from sys import exit
+import ctypes
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 import numpy as np
 
-# ! Extract key to .env file
-
-# ? MessageBox format CTYPES
+# ? MessageBox format CTYPES for popup windows
 # ctypes.windll.user32.MessageBoxW(0, "Your text", "Your title", 1)
+
+# Get environment variable to get API key
 load_dotenv()
 ALPHA_KEY = os.getenv("ALPHA_KEY")
+
 
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow, self).__init__()
-        
+
         # Attributes
-        self.ticker = "TSLA" # Ticker symbol
+        self.ticker = "TSLA"  # Ticker symbol
         self.money = 0
         self.log = []
         self.isInvested = False
         self.buyTolerance = 1
         self.sellTolerance = 1
 
-        
         loadUi("StockGUI.ui", self)
-        
-        # Connections
-        self.exitButton.clicked.connect(self.kill)
-        self.GetStockDataButton.clicked.connect(self.doStuff)
 
+        # Connections to callback functions
+        self.exitButton.clicked.connect(self.kill)
+        self.GetStockDataButton.clicked.connect(self.evaluateStrategy)
+        self.tickerEdit.textChanged.connect(self.updateTicker)
+
+    def updateTicker(self):
+        """Updates ticker symbol from GUI input box"""
+        self.ticker = self.tickerEdit.text()
 
     def logActions(self, price, buysell):
+        """Log a buy/sell action to the log.
+        Showing current price, money, action and resulting money"""
+
         roundMoney = round(self.money, 2)
         roundBuy = round(self.money + price, 2)
         roundSell = round(self.money - price, 2)
         logMsg = ""
+
         if buysell == "S":
             logMsg = f"{roundMoney} now.Sold at ${price} => {roundBuy}"
         else:
             logMsg = f"{roundMoney} now. Bought at ${price} => {roundSell}"
+
         self.log.append(logMsg)
 
-    def doStuff(self):
-        periods = 60
-        ts = TimeSeries(key=ALPHA_KEY, output_format='pandas')
-        data_ts, meta_data_ts = ts.get_weekly(symbol='GOOG')
-        data_otn = data_ts.iloc[::-1]
-        # data_otn.head()
-        data = data_otn['4. close'].to_frame() # Isolate to closing column
-        data['SMA30'] = data['4. close'].rolling(30).mean() # Calculate 30 week moving average
-        data.dropna(inplace = True)
+    def evaluateStrategy(self):
+        """Gets stock data and runs
+        through data to evaluate trading strategy."""
+
+        # Get weekly timeseries data for given stock and put into pandas dataframe
+        ts = TimeSeries(key=ALPHA_KEY, output_format="pandas")
+        data_ts, meta_data_ts = ts.get_weekly(symbol=self.ticker)
+        data_otn = data_ts.iloc[::-1]  # Reverse to order from old to recent
+
+        data = data_otn["4. close"].to_frame()  # Isolate to closing column
+        data["SMA30"] = (
+            data["4. close"].rolling(30).mean()
+        )  # Calculate 30 week moving average
+        data.dropna(inplace=True)  # Remove NA (null) values
+
+        # Parameters for plot
         plt.rcParams["figure.figsize"] = [16, 8]
-        plt.plot(data[['4. close', 'SMA30']])
+        plt.plot(data[["4. close", "SMA30"]])
 
-        isInvested = False # Assume you start with no money invested
-        money = 0
-        log = []
+        # Loop through and check for buy or sell conditions
+        for i in range(len(data["SMA30"])):
+            currentPrice = data["4. close"][i]
+            currentSMA = data["SMA30"][i]
 
-        moneySpent = 0
+            underSMA = currentPrice < currentSMA
+            overSMA = currentPrice > currentSMA
 
-        buyTolerance = 0  #  0.05
-        sellTolerance = 1 #0.15
-        def logAction(price, buysell):
-            if buysell == "S":
-                log.append(f"{round(money, 2)} now.Sold at ${price} => {round(money + price, 2)}")
-            else:
-                log.append(f"{round(money, 2)} now. Bought at ${price} => {round(money - price, 2)}")
-            
-        # Loop through and check if price < sma or not
-        for i in range(len(data['SMA30'])):
-            currentPrice = data['4. close'][i]
-            currentSMA = data['SMA30'][i]
-            
-            if currentPrice < currentSMA and not isInvested and currentPrice <= (1 - buyTolerance) * currentSMA:
-                plt.plot([data.index[i]], [currentSMA], marker = "$B$", ls = 'none', ms = 10, color = "red")
-                isInvested = True
-                logAction(currentPrice, "B")
-                money -= currentPrice
-                
-            elif currentPrice > currentSMA and isInvested and currentPrice >= sellTolerance * currentSMA:
-                plt.plot([data.index[i]], [currentSMA], marker = "$S$", ls = 'none', ms = 10, color = "lime")
-                isInvested = False
-                logAction(currentPrice, "S")
-                money += currentPrice
-        for entry in log:
+            underBuyTolerance = currentPrice <= self.buyTolerance * currentSMA
+            overSellTolerance = currentPrice >= self.sellTolerance * currentSMA
+
+            if underSMA and underBuyTolerance and not self.isInvested:
+                plt.plot(
+                    [data.index[i]],
+                    [currentSMA],
+                    marker="$B$",
+                    ls="none",
+                    ms=10,
+                    color="red",
+                )
+                self.isInvested = True
+                self.logActions(currentPrice, "B")
+                self.money -= currentPrice
+
+            elif overSMA and overSellTolerance and self.isInvested:
+                plt.plot(
+                    [data.index[i]],
+                    [currentSMA],
+                    marker="$S$",
+                    ls="none",
+                    ms=10,
+                    color="lime",
+                )
+                self.isInvested = False
+                self.logActions(currentPrice, "S")
+                self.money += currentPrice
+
+        # Print log to console
+        for entry in self.log:
             print(entry)
+
         plt.show()
+
     def kill(self):
         """Exit the Interface"""
         print(ALPHA_KEY)
-        exit(0)
+        sys.exit(0)
 
-app=QApplication(sys.argv)
-mainwindow=MainWindow()
-widget=QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedWidth(1000)
-widget.setFixedHeight(700)
-widget.show()
-sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainwindow = MainWindow()
+    widget = QtWidgets.QStackedWidget()
+    widget.addWidget(mainwindow)
+    widget.setFixedWidth(1000)
+    widget.setFixedHeight(700)
+    widget.show()
+    sys.exit(app.exec_())
